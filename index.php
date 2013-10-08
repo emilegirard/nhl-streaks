da59aa780f6300dcbe4b54b1dec2e0a9d37ed510
Emile Girard <emilegirard@gmail.com>
2013-10-08 14:25:56 -0400

    Index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('includes/config.php');
 ?>
 <html>
 <head>
-	<title>NB of games played in interval</title>
+	<title>NHL - Teams &amp; Players streaks</title>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
@@ -30,7 +30,11 @@ include('includes/config.php');
 <body>
 	<a href="https://github.com/emilegirard/nhl-streaks"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub"></a>
 
-
+	<h1>Actions</h1>
+	<ul>
+		<li><a href="?action=update">Update Data</a></li>
+		<li><a href="?action=view">View</a></li>
+	</ul>
 
 </body>
 </html>
