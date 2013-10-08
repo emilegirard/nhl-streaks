52cdb7772cdfad4c8e8aee031419f8ff8d84e68c
Emile Girard <emilegirard@gmail.com>
2013-10-08 14:43:09 -0400

    Fix paths

--- includes/config.php
+++ includes/config.php
@@ -14,4 +14,12 @@ include('simple-html-dom.class.php');
 define('PATH', 			dirname(dirname (__FILE__)) );
 define('PATH_CACHE', 	PATH . '/cache');
 define('PATH_INC',		PATH . '/includes');
+define('PATH_ASSETS',	PATH . '/assets');
+define('PATH_JS',		PATH_ASSETS . '/js');
+define('PATH_CSS',		PATH_ASSETS . '/css');
 define('CACHE_EXPIRE',	3600);
+
+define('URI',			'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+define('URI_ASSETS', 	URI . 'assets');
+define('URI_CSS', 		URI_ASSETS . '/css');
+define('URI_JS', 		URI_ASSETS . '/js');
--- index.php
+++ index.php
@@ -9,32 +9,27 @@ include('includes/config.php');
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-	<link rel="stylesheet" href='assets/css/dark-hive/jquery-ui-1.10.3.custom.min.css' type='text/css' media='all' />
+	<script src="<?=URI_JS;?>/nhl-streaks.js"></script>
 
-	<script>
-	  $(function() {
-
-
-	  });
-	</script>
+	<link rel="stylesheet" href='<?=URI_CSS;?>/dark-hive/jquery-ui-1.10.3.custom.min.css' type='text/css' media='all' />
+	<link rel="stylesheet" href='<?=URI_CSS;?>/nhl-streaks.css' type='text/css' media='all' />
 
 </head>
 <style type="text/css">
-	body {
-		background-color:#efefef;
-		font-size:100%;
-		font-family:'courier new', courier, arial;
-		width:100%;
-	}
+
 </style>
 <body>
 	<a href="https://github.com/emilegirard/nhl-streaks"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub"></a>
 
 	<h1>Actions</h1>
 	<ul>
-		<li><a href="?action=update">Update Data</a></li>
-		<li><a href="?action=view">View</a></li>
+		<li><a href="?ajax=true&action=update">Update Data</a></li>
+		<li><a href="?ajax=true&action=view">View</a></li>
 	</ul>
+
+	<div id="result">
+
+	</div>
 
 </body>
 </html>
